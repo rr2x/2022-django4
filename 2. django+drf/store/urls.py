6dbb366832f42eb4f2e793b244378b3aa89e1f5a
rr2x <79620740+rr2x@users.noreply.github.com>
2022-03-28 15:08:54 +0800

    update 2

--- 2. django+drf/store/urls.py
+++ 2. django+drf/store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('products/', views.product_list),
     # converter; <int:...>
     path('products/<int:id>/', views.product_detail),
+
 ]
--- 2. django+drf/store/views.py
+++ 2. django+drf/store/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
+from .models import Product
+from .serializers import ProductSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    return Response('hey')
+    if request.method == 'GET':
+        # select_related = preload collection table so that
+        # it bypasses lazy loading and prevent too much queries
+        queryset = Product.objects.select_related('collection').all()
+        # many=True cycle through all data
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        # deserialize data
+        serializer = ProductSerializer(data=request.data)
+        # serializer.validated_data
+        return Response('ok')
+
 
 @api_view()
 def product_detail(request, id):
-    return Response(f'ho, {id}')
+    # try:
+    #     product = Product.objects.get(pk=id)
+    #     serializer = ProductSerializer(product)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get_object_or_404() wraps the try-catch block above
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, id):
+    return Response('ok')
