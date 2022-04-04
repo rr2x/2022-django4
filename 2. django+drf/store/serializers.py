79d35565a8aa39b7a5ff4b5e3e9fec0ed7050edc
rr2x <79620740+rr2x@users.noreply.github.com>
2022-04-04 13:27:57 +0800

    update 10

--- 2. django+drf/store/serializers.py
+++ 2. django+drf/store/serializers.py
@@ -121,7 +121,7 @@ class CartSerializer(serializers.ModelSerializer):
 
 class CustomerSerializer(serializers.ModelSerializer):
     # user_id is created at runtime, so explicitly define it
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Customer
