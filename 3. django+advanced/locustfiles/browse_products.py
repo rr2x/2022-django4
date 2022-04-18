b2155247b5b8d61b4dd27d9be639fe0730ee6289
rr2x <79620740+rr2x@users.noreply.github.com>
2022-04-18 11:02:20 +0800

    update 5

---
+++ 3. django+advanced/locustfiles/browse_products.py
@@ -0,0 +1,41 @@
+from locust import HttpUser, task, between
+from random import randint
+
+
+class WebsiteUser(HttpUser):
+    # randomly wait between 1-5 second on each task
+    wait_time = between(1, 5)
+
+    @task(2)
+    def view_products(self):
+        print('view products - locust')
+        collection_id = randint(2, 6)
+        self.client.get(
+            f'/store/products/?collection_id={collection_id}',
+            name='/store/products')
+
+    # add weight, meaning this has more probability that user access this endpoint
+    # higher value indicates higher execution
+    @task(4)
+    def view_product(self):
+        print('view product details - locust')
+        product_id = randint(1, 1000)
+        self.client.get(
+            f'/store/products/{product_id}',
+            name='/store/products/:id')
+
+    @task(1)
+    def add_to_cart(self):
+        print('add to cart - locust')
+        product_id = randint(1, 10)
+        self.client.post(
+            f'/store/carts/{self.cart_id}/items/',
+            name='/store/carts/items',
+            json={'product_id': product_id, 'quantity': 1}
+        )
+
+    # lifecycle hook, not a task
+    def on_start(self):
+        response = self.client.post('/store/carts/')
+        result = response.json()
+        self.cart_id = result['id']
--- 3. django+advanced/store/tests/test_collections.py
+++ 3. django+advanced/store/tests/test_collections.py
@@ -2,42 +2,69 @@
 #   be descriptive on test names to tell what behavior we are testing
 #   AAA; Arrange -> Act -> Assert
 
-from django.contrib.auth.models import User
+from store.models import Collection, Product
 from rest_framework import status
-from rest_framework.test import APIClient
 import pytest
+from model_bakery import baker  # to easily create objects
+
+# note = pytest always creates "test_<database_name>" database
+# and after running all tests, it drop that database
+
+
+# define local fixture that is only used here
+# api_client fixture is extracted from conftest.py
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection  # return a function, closure pattern
 
 
 # all methods will inherit this decorator
 @pytest.mark.django_db
 class TestCreateCollection:
+
     # @pytest.mark.skip    # use this decorator if you want to skip this test for now
-    def test_if_user_is_anonymous_returns_401(self):
-        # Arrange
-
-        # Act
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
-
-        # Assert (check if we got the behavior we expect)
+    # create_collection is a local fixture
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
+        # arrange
+        # act
+        response = create_collection({'title': 'a'})
+        # assert (check if we got the behavior we expect)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self):
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'a'})
+    # authenticate global fixture is extracted from conftest.py
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+        # arrange
+        authenticate()
+        # act
+        response = create_collection({'title': 'a'})
+        # assert (check if we got the behavior we expect)
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_returns_400(self):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': ''})
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+        response = create_collection({'title': ''})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_returns_201(self):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': 'a'})
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+        response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
